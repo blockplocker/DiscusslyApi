0ba0ae198018777169e83ab6e06578180201dbe3
blockplocker <noahallklint@gmail.com>
2025-05-26 17:09:37 +0200

    Added migration for changed datatypes

--- DiscusslyApi/Migrations/20250526144840_ChangeDatatypeForComment.Designer.cs
+++ DiscusslyApi/Migrations/20250526144840_ChangeDatatypeForComment.Designer.cs
@@ -4,6 +4,7 @@ using DiscusslyApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DiscusslyApi.Migrations
 {
     [DbContext(typeof(DiscusslyApiContext))]
-    partial class DiscusslyApiContextModelSnapshot : ModelSnapshot
+    [Migration("20250526144840_ChangeDatatypeForComment")]
+    partial class ChangeDatatypeForComment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
